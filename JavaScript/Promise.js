0779ac8dccc65c224269c6454a4dc80e20cd3196
朱文涛 <zhuwentao@koolearn.com>
2020-04-03 12:02:04 +0800

    feat:增加promise

---
+++ JavaScript/Promise.js
@@ -0,0 +1,10 @@
+// let p1 = Promise.resolve('1')
+// let p2 = Promise.resolve('2')
+
+// Promise.all([p1, p2]).then(console.log)
+
+// let p1 = Promise.resolve('1')
+// let p2 = Promise.reject('2')
+
+// Promise.all([p1, p2]).then(console.log, console.log)
+
